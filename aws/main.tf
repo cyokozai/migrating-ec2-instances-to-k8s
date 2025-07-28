33f9e20735579c88aea4b0c4072928272b76be17
cyokozai <yuu4suke27@outlook.jp>
2025-07-28 16:33:31 +0900

    AWSリソースのTerraform構成を追加し、NginxをインストールするEC2インスタンスを設定。VPC、サブネット、セキュリティグループ、出力変数、変数を定義し、タグを統一。

--- aws/main.tf
+++ aws/main.tf
@@ -36,8 +36,7 @@ resource "aws_instance" "web_server" {
   subnet_id     = aws_subnet.public.id
   vpc_security_group_ids = [aws_security_group.web_sg.id]
   key_name      = var.ec2_key_name
-
-  # 起動時にNginxをインストールして起動するスクリプト
+  
   user_data = <<-EOF
               #!/bin/bash
               yum update -y
